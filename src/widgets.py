fdf0dfcab86c629b4ed4ed4771a48b536e416719
Christian Aucane <christianaucane1@gmail.com>
2024-06-03 11:55:15 +0200

    Create ImageButton class

--- src/widgets.py
+++ src/widgets.py
@@ -1,97 +1,44 @@
 import pygame
 
+from constants import BUTTONS_IMAGES_DIR, FONT_PATH, BLACK
 
-class Button:
-    """
-    Button class for creating interactive buttons on a Pygame screen.
 
-    Args:
-        screen (pygame.Surface): The screen to draw the button on.
-        x (int): The x coordinate of the button.
-        y (int): The y coordinate of the button.
-        width (int): The width of the button.
-        height (int): The height of the button.
-        text (str): The text displayed on the button.
-        bg_color (tuple): The color of the button.
-        text_color (tuple): The color of the text.
-
-    Attributes:
-        rect (pygame.Rect): The rectangle representing the button.
-        text (str): The text displayed on the button.
-        bg_color (tuple): The color of the button.
-        text_color (tuple): The color of the text.
-        screen (pygame.Surface): The screen to draw the button on.
-        x (int): The x coordinate of the button.
-        y (int): The y coordinate of the button.
-        width (int): The width of the button.
-        height (int): The height of the button.
-
-    Methods:
-        draw(): Draw the button on the screen.
-        is_clicked(pos): Check if the button is clicked given a mouse position.
-        set_bg_color(color): Change the button color.
-        set_text_color(color): Change the button text color.
-    """
-    def __init__(self, screen, x, y, width, height, text, bg_color, text_color):
-        """
-        Initialize the button
-
-        Args:
-            screen (pygame.Surface): the screen to draw on
-            x (int): x coordinate of the button
-            y (int): y coordinate of the button
-            width (int): width of the button
-            height (int): height of the button
-            text (str): text of the button
-            bg_color (tuple): color of the button
-            text_color (tuple): color of the text
-        """
+class BaseButton:
+    def __init__(self, screen, x, y, width, height, text, data=None, text_color=BLACK, font_path=FONT_PATH, font_size=30):
+        self.screen = screen
         self.rect = pygame.Rect(x, y, width, height)
         self.text = text
-        self.bg_color = bg_color
+        self.data = data
+        self.font = pygame.font.Font(font_path, font_size)
         self.text_color = text_color
-        self.screen = screen
-        self.x = x
-        self.y = y
         self.width = width
         self.height = height
+        self.x = x
+        self.y = y
 
     def draw(self):
-        """
-        Draw the button on the screen
-        """
-        pygame.draw.rect(self.screen, self.bg_color, self.rect)
-        font = pygame.font.SysFont("", 30)
-        text_surface = font.render(self.text, True, self.text_color)
+        raise NotImplementedError("draw method must be implemented in subclass")
+    
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos)
+    
+    def draw_text(self):
+        text_surface = self.font.render(self.text, True, self.text_color)
         text_rect = text_surface.get_rect(center=self.rect.center)
         self.screen.blit(text_surface, text_rect)
 
-    def is_clicked(self, pos):
-        """
-        Check if the button is clicked given a mouse position
-
-        Args:
-            pos (tuple): the position of the mouse
-
-        Returns:
-            bool: True if the button is clicked, False otherwise
-        """
-        return self.rect.collidepoint(pos)
-
-    def set_bg_color(self, color):
-        """
-        Change the button color
-
-        Args:
-            color (tuple): the new color
-        """
-        self.bg_color = color
-
     def set_text_color(self, color):
-        """
-        Change the button text color
-
-        Args:
-            color (tuple): the new color
-        """
         self.text_color = color
+
+
+class ImageButton(BaseButton):
+    def __init__(self, screen, x, y, width, height, text, background_image_file, data=None, text_color=BLACK, font_path=FONT_PATH, font_size=30):
+        super().__init__(screen, x, y, width, height, text, data, text_color, font_path, font_size)
+        self.background_image = pygame.transform.scale(pygame.image.load(BUTTONS_IMAGES_DIR /background_image_file), (self.width, self.height))
+
+    def draw_background_image(self):
+        self.screen.blit(self.background_image, self.rect)
+    
+    def draw(self):
+        self.draw_background_image()
+        self.draw_text()
