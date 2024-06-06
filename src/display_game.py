0092fd93438ac9c0b64aabec4e279b96ad476220
Christian Aucane <christianaucane1@gmail.com>
2024-06-06 14:13:16 +0200

    Put settings in MenuScreen

--- src/display_game.py
+++ src/display_game.py
@@ -8,7 +8,6 @@ from constants import Sizes, Colors, Paths
 from display.game import GameScreen
 from display.menu import MenuScreen
 from display.create import CreateScreen
-from display.settings import SettingsScreen
 from utils.score_manager import ScoreManager
 from utils.sound_manager import SoundManager
 
@@ -51,8 +50,6 @@ class SokobanApp:
         if screen_name == "menu":
             pygame.time.delay(600)
             self.current_screen = MenuScreen(self, self.screen)
-        elif screen_name == "settings":
-            self.current_screen = SettingsScreen(self, self.screen)
         elif screen_name == "game":
             self.current_screen = GameScreen(self, self.screen, self.score_manager)
         elif screen_name == "create":
