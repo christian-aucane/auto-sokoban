7d1d9d0020bc4afb53fb464fc97ffb8d00e2ba6b
Christian Aucane <christianaucane1@gmail.com>
2024-06-06 14:04:27 +0200

    Add TODOs

--- src/utils/score_manager.py
+++ src/utils/score_manager.py
@@ -2,18 +2,20 @@ import pandas as pd
 
 
 class ScoreManager:
-    COLUMNS = ["Player", "Grid", "Moves Count", "Reset Count", "Cancel Count", "Solve Used", "Execution Time"]
+    COLUMNS = ["Player Name", "Grid Name", "Moves Count", "Reset Count", "Cancel Count", "Solve Used", "Execution Time"]
     def __init__(self, score_file_path=None):
+        
+        self.score_df = pd.DataFrame(columns=self.COLUMNS)
         if score_file_path is not None:
-            self.score_df = pd.read_csv(score_file_path)
-        else:
-            self.score_df = pd.DataFrame(columns=self.COLUMNS)
+            if score_file_path.exists():
+                self.score_df = pd.read_csv(score_file_path)
         self.score_file_path = score_file_path
 
     def add_score(self, player_name, level):
         stats = level.stats
         stats["Player Name"] = player_name
-        self.score_df = self.score_df._append(stats, ignore_index=True)
+        # TODO : message d'avertissement
+        self.score_df = pd.concat([self.score_df, pd.DataFrame(stats, index=[0])], ignore_index=True)
 
     def save_scores(self):
         self.score_df.to_csv(self.score_file_path, index=False)
